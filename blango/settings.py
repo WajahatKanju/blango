1b45c3b7c32cf2be101b1530dd3f2fbcf752ff70
Wajahat Ahmad <wajahat5amad@gmail.com>
2023-02-23 20:59:56 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -29,6 +29,11 @@ class Dev(Configuration):
   SESSION_COOKIE_SAMESITE = 'None'
   ACCOUNT_ACTIVATION_DAYS = 7
   EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+  SITE_ID = 1
+  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+  ACCOUNT_EMAIL_REQUIRED = True
+  ACCOUNT_USERNAME_REQUIRED = False
+  ACCOUNT_AUTHENTICATION_METHOD = "email"
   # Application definition
 
   INSTALLED_APPS = [
@@ -37,9 +42,14 @@ class Dev(Configuration):
       'django.contrib.contenttypes',
       'django.contrib.sessions',
       'django.contrib.messages',
+      'django.contrib.sites',
       'django.contrib.staticfiles',
 
       # Third Party 
+      'allauth',
+      'allauth.account',
+      'allauth.socialaccount',
+      'allauth.socialaccount.providers.google',
       "debug_toolbar",
       'crispy_forms',
       'crispy_bootstrap5',
