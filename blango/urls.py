e85703b9b848d0ec1e0462b148a4f1d73f8b967d
Wajahat Ahmad <wajahat5amad@gmail.com>
2023-04-14 01:07:56 +0000

    Finish First-Party  Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -41,6 +41,8 @@ urlpatterns = [
             RegistrationView.as_view(form_class=BlangoRegistrationForm),
             name="django_registration_register",
         ),
+    path("api/v1/", include("blog.api_urls")),
+
 
 
 
