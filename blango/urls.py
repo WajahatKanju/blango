58cc72527bcd073e9cf45f40686ea663cc70ab7c
Wajahat Ahmad <wajahat5amad@gmail.com>
2023-04-19 00:55:01 +0000

    Finish DRF views

--- blango/urls.py
+++ blango/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
             RegistrationView.as_view(form_class=BlangoRegistrationForm),
             name="django_registration_register",
         ),
-    path("api/v1/", include("blog.api_urls")),
+    path("api/v1/", include("blog.api.urls")),
 
 
 
